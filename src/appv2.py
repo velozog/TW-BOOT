28fcbc2d37bf2a73323c99591eee7712e5e453f4
Geziel <gezielzinho@gmail.com>
2024-07-11 21:18:44 -0300

    adicionado a função de Anti-Boot

--- src/appv2.py
+++ src/appv2.py
@@ -1,5 +1,5 @@
 import pyautogui as pg
-from time import sleep
+from time import sleep, time
 from datetime import datetime, timedelta
 from termcolor import colored
 import colorama
@@ -15,9 +15,6 @@ colorama.init(autoreset=True)
 TEXTO = colored("TW BOOT", "green")
 CAMINHO_IMAGEM = os.path.join(PATH, "imagens/tropas_insuficiente.png")
 print(colored(f"Inicializando {TEXTO}\n", "cyan"))
-pg.PAUSE = 0.5
-
-
 
 def enviar_mensagem_whatsapp(numero, mensagem) -> bool:
     try:
@@ -35,8 +32,9 @@ def manda_msg(mensagem):
     while not enviar_mensagem_whatsapp(NUMERO_TELEFONE, mensagem) or repet == 3:
         sleep(10)
         repet += 1
+    sleep(2)
     pg.hotkey("alt", "f4")
-    sleep(3)
+    sleep(1)
     pg.press("enter")
         
             
@@ -44,27 +42,33 @@ def manda_msg(mensagem):
 def mandar_tropasv2():
     path_img_1 = os.path.join(PATH, "imagens/botao_1.png")
     path_img_2 = os.path.join(PATH, "imagens/botao_2.png")
+    path_img_3 = os.path.join(PATH, "imagens/aint_boot.png.png")
     pg.click(x=900, y=630)
+    sleep(1)
     pg.scroll(-500)
+    sleep(0.5)
 
     while True:
         try:
             img = pg.locateCenterOnScreen(image=path_img_1)
             img2 = pg.locateCenterOnScreen(image=path_img_2)
+            img3 = pg.locateCenterOnScreen(image=path_img_3)
             if img:
-                sleep(0.3)
+                # sleep(0.2)
                 pg.click(img.x, img.y)
                 print(colored("Tropas enviadas com sucesso!!", "green"))
-            if img2:
-                sleep(0.3)
+            else:
+                # sleep(0.2)
                 pg.click(img2.x, img2.y)
                 print(colored("Tropas enviadas com sucesso!!", "green"))
 
         except pg.ImageNotFoundException as err:
             print(f"Procurando {err}")
+            pg.scroll(-300)
         try:
             if pg.locateCenterOnScreen(image=CAMINHO_IMAGEM):
                 return colored("Tropas insuficiente", "red")
+           
 
         except:
             print(colored("Ainda tem tropas na aldeia", "green"))
@@ -75,8 +79,6 @@ def trocar_aldeia(num_aldeia: str):
         pg.press(num_aldeia)
         return "Retormando a aldeia inicial"
     pg.press(num_aldeia)
-    sleep(1)
-
     return "Indo até a proxima aldeia"
 
 
@@ -94,15 +96,13 @@ def proxima_execucao(tempo):
     colored(print(f"Vamos aguardar {tempo} minutos"), "yellow")
     return f"Proxima Execução {data_hora_futura.strftime("%d/%m/%Y %H:%M")}"
 
-
 def main():
+    
     while True:
-        
         data_hora_atual = datetime.now().strftime("%d/%m/%Y %H:%M")
-
-        tempo = randint(30, 40)
-        msg_inicio = f"Script iniciando {data_hora_atual}"
-        mensagem = f"Script funcionando corretamente, finalizado as {data_hora_atual}, {proxima_execucao(tempo)}"
+        tempo = randint(20, 25)
+        msg_inicio = f"{TEXTO} iniciado em {data_hora_atual}"
+        print(colored(msg_inicio, "blue"))
         # print(colored(manda_msg(msg_inicio), "green"))
         print(colored(mandar_tropasv2(), "red"))
         print(colored(trocar_aldeia("2"), "yellow"))
@@ -112,7 +112,9 @@ def main():
         print(colored(trocar_aldeia("4"), "yellow"))
         print(colored(mandar_tropasv2(), "red"))
         print(colored(trocar_aldeia("1"), "yellow"))
-        print(colored(proxima_execucao(tempo), "cyan"))
+        data_hora_atual2 = datetime.now().strftime("%d/%m/%Y %H:%M")
+        mensagem = f"Script funcionando corretamente, finalizado em {data_hora_atual2}, {proxima_execucao(tempo) } "
+        print(colored(mensagem, "cyan"))
         # print(colored(manda_msg(mensagem), "green"))
         print(colored(pause_tempo(tempo), "light_yellow", on_color="on_dark_grey"))
 
--- src/teste.py
+++ src/teste.py
@@ -1,25 +1,52 @@
-import os
-import pywhatkit as kit
 import pyautogui as pg
-from time import sleep
-import pyautogui as pg
-from time import sleep
-from datetime import datetime, timedelta
-from termcolor import colored
-import colorama
-from random import random, randint
+import time
+
+
+# def scroll_and_switch_page(scroll_amount, wait_time, max_scroll_attempts):
+#     # Definindo o número máximo de tentativas de scroll
+#     scroll_attempts = 0
+
+#     while scroll_attempts < max_scroll_attempts:
+#         # Tenta dar scroll para baixo
+#         pyautogui.scroll(-scroll_amount)
+#         time.sleep(wait_time)  # Espera um pouco para a página carregar
+
+#         # Verifica se o scroll teve efeito
+#         current_scroll_position = pyautogui.position()
+#         pyautogui.scroll(scroll_amount)  # Desfaz o scroll para verificar a posição
+#         time.sleep(wait_time)
+#         new_scroll_position = pyautogui.position()
+
+#         if new_scroll_position == current_scroll_position:
+#             # Se a posição não mudou, muda de página
+#             # Aqui você deve adicionar o código para mudar de página
+#             print("Mudando de página...")
+#             # Por exemplo, você pode clicar em um botão de próxima página
+#             # pyautogui.click(x, y)  # Adicione as coordenadas do botão de próxima página
+#             scroll_attempts += 1  # Aumenta o número de tentativas de scroll
+#         else:
+#             # Se a posição mudou, aumenta o número de tentativas de scroll
+#             scroll_attempts = 0
+
+
+# # Defina os parâmetros
+# scroll_amount = 100  # Quantidade de scroll para baixo
+# wait_time = 2  # Tempo para esperar o carregamento da página
+# max_scroll_attempts = (
+#     3  # Número máximo de tentativas de scroll antes de mudar de página
+# )
+
+# scroll_and_switch_page(scroll_amount, wait_time, max_scroll_attempts)
 import os
-import pywhatkit as kit
 
-
-def proxima_execucao(tempo):
-    data_hora_atual = datetime.now()
-    data_hora_futura = data_hora_atual + timedelta(minutes=tempo)
-    colored(print(f"Vamos aguardar {tempo} minutos"), "yellow")
-    return f"Proxima Execução {data_hora_futura.strftime("%d/%m/%Y %H:%M")}"
-
-
-tempo = 30
-
-mensagem = f"Tudo Sub Tranquilo {proxima_execucao(tempo)}"
-print(mensagem)
+PATH = os.path.dirname(__file__)
+path_img_3 = os.path.join(PATH, "imagens/aint_boot.png")
+while True:
+    try:
+        img3 = pg.locateCenterOnScreen(image=path_img_3)
+        if img3:
+            pg.click(img3.x, img3.y)
+            print("Fudeu anti Boot na area")
+            break
+    except pg.ImageNotFoundException:
+        print("Procurando")
